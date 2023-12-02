e78a899686c61e912c713ba0fcd21b6738a63c47
Noar <arnaud.moretti@protonmail.com>
2023-12-02 11:00:59 +0100

    day one

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace AdventOfCode2023;
+﻿using Day1;
+
+namespace AdventOfCode2023;
 
 internal class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        string dataPath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly()!.Location)!, "DayInputs");
+        Console.WriteLine("************** Day 1 example 1 ******************");
+        var calib = new Calibration(Path.Combine(dataPath, "Day1Example1.txt"));
+        Console.WriteLine($"Example 1 result : {calib.GetCalibrationSum()}");
+        calib = new Calibration(Path.Combine(dataPath, "Day1Input1.txt"));
+        Console.WriteLine($"First part result : {calib.GetCalibrationSum()}");
+        calib = new Calibration(Path.Combine(dataPath, "Day1Example2.txt"));
+        Console.WriteLine($"Example 2 result : {calib.GetCalibrationSum(true)}");
+        calib = new Calibration(Path.Combine(dataPath, "Day1Input1.txt"));
+        Console.WriteLine($"Second part result : {calib.GetCalibrationSum(true)}");
     }
 }
---
+++ Day1/Calibration.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    public partial class Calibration
+    {
+        private readonly string[] _rawCalibrationValues;
+        public Calibration(string inputFilePath)
+        {
+            try
+            {
+                _rawCalibrationValues = File.ReadAllLines(inputFilePath);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public int GetCalibrationSum(bool includeSpelledDigit = false)
+        {
+            return includeSpelledDigit ? GetCalibrationWithSpelledValue().Sum() : GetCalibrationValue().Sum();
+        }
+
+
+
+        private List<int> GetCalibrationValue()
+        {
+            var output = new List<int>();
+            var regex = RawCalibRegex();
+            foreach (string rawCalibText in _rawCalibrationValues)
+            {
+                string firstDigitText = regex.Matches(rawCalibText).First().Value;
+                string lastDigitText = regex.Matches(rawCalibText).Last().Value;
+                if (int.TryParse(firstDigitText + lastDigitText, out int calibValue))
+                {
+                    output.Add(calibValue);
+                }
+            }
+            return output;
+        }
+
+        private List<int> GetCalibrationWithSpelledValue()
+        {
+            var output = new List<int>();
+            var regex = RawCalibWithSpelledDigitRegex();
+            foreach (string rawCalibText in _rawCalibrationValues)
+            {
+                string firstDigitText = regex.Matches(rawCalibText).First().Groups[1].Value;
+                int calibValue;
+                if (int.TryParse(firstDigitText, out int tens))
+                {
+                    calibValue = tens * 10;
+                }
+                else
+                {
+                    calibValue = IntifySpelledDigit(firstDigitText) * 10;
+                }
+                string lastDigitText = regex.Matches(rawCalibText).Last().Groups[1].Value;
+                if (int.TryParse(lastDigitText, out int unit))
+                {
+                    calibValue += unit;
+                }
+                else
+                {
+                    calibValue += IntifySpelledDigit(lastDigitText);
+                }
+                Console.WriteLine($"{rawCalibText} -> {calibValue}");
+                output.Add(calibValue);
+            }
+            
+            return output;
+        }
+
+        private int IntifySpelledDigit(string spelledDigit) =>
+            spelledDigit switch
+            {
+                "one" => 1,
+                "two" => 2,
+                "three" => 3,
+                "four" => 4,
+                "five" => 5,
+                "six" => 6,
+                "seven" => 7,
+                "eight" => 8,
+                "nine" => 9,
+                _ => throw new InvalidCastException($"{spelledDigit} is not a valid number")
+            };
+
+
+        [GeneratedRegex(@"\d")]
+        private static partial Regex RawCalibRegex();
+        [GeneratedRegex(@"(?=(\d|one|two|three|four|five|six|seven|eight|nine))")]
+        private static partial Regex RawCalibWithSpelledDigitRegex();
+    }
+}
